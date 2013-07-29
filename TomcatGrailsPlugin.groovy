a3e2d03e823f91bc07405b4a21936b9c74623fe4
Jeff Scott Brown <jbrown@gopivotal.com>
2013-07-29 10:10:47 -0500

    set version to 2.2.4

--- TomcatGrailsPlugin.groovy
+++ TomcatGrailsPlugin.groovy
@@ -13,7 +13,7 @@
  * limitations under the License.
  */
 class TomcatGrailsPlugin {
-    def version = "2.2.4.BUILD-SNAPSHOT"
+    def version = "2.2.4"
     def grailsVersion = "2.2 > *"
     def scopes = [excludes: 'war']
     def author = "Graeme Rocher"
