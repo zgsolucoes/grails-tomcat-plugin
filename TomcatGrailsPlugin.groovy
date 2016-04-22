f4aae68fb4c7ec4a59a7181a89d74302fb8ba6f8
Jeff Scott Brown <brownj@ociweb.com>
2016-04-22 09:31:52 -0500

    Use Tomcat 7.0.69
    Related to grails/grails-core#9891

--- TomcatGrailsPlugin.groovy
+++ TomcatGrailsPlugin.groovy
@@ -13,7 +13,7 @@
  * limitations under the License.
  */
 class TomcatGrailsPlugin {
-    def version = "7.0.55.3"
+    def version = "7.0.69-SNAPSHOT"
     def grailsVersion = "2.3 > *"
     def scopes = [excludes: 'war']
     def author = "Graeme Rocher"
