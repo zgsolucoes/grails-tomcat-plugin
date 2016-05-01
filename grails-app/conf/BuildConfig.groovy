b64bc5b3098b9f13cce24e5016931885fd0f571d
Burt Beckwith <burt@burtbeckwith.com>
2016-04-30 21:27:26 -0400

    update to Tomcat 8.0.33; fixes #15

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -18,7 +18,7 @@ grails.project.dependency.resolution = {
 	}
 
 	dependencies {
-		String tomcatVersion = '8.0.30'
+		String tomcatVersion = '8.0.33'
 		compile "org.apache.tomcat.embed:tomcat-embed-core:$tomcatVersion"
 		['el', 'jasper', 'logging-log4j', 'logging-juli', 'websocket'].each {
 			runtime "org.apache.tomcat.embed:tomcat-embed-$it:$tomcatVersion"
